f1ea6570dfcfb25b7d8b55e2532a5e6d034e719e
Ali Kehel <ali.saber.27.2@gmail.com>
2022-09-10 22:40:44 +0200

    init commit

---
+++ App.js
@@ -0,0 +1,14 @@
+// import * as React from "react";
+// import axios from "axios";
+// import AsyncStorage from "@react-native-async-storage/async-storage";
+// import { StatusBar } from "expo-status-bar";
+// import { StyleSheet, Text, View, Button, TouchableOpacity, SafeAreaViewBase } from "react-native";
+// import { useState, useEffect } from "react";
+// import { NavigationContainer } from "@react-navigation/native";
+// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+export default function App() {
+    return <></>;
+}
+
+const styles = StyleSheet.create({});
